43ad44c3b284185a4b08cb87a3ae2c371ee8fde4
ashigirl96 <s1511392@u.tsukuba.ac.jp>
2018-03-23 09:36:24 +0900

    Solve exercise01

--- exercises/solution01.py
+++ exercises/solution01.py
@@ -11,45 +11,44 @@ import ray
 
 @ray.remote
 def slow_function(i):
-	"""This function is a proxy for a more interesting and computationally
-	intensive function.
-	"""
-	time.sleep(1)
-	return i
+  """This function is a proxy for a more interesting and computationally
+  intensive function.
+  """
+  time.sleep(1)
+  return i
 
 
 def measure_duration():
-	"""Sleep a little to improve the accuracy of the timing measurements below.
-	We do this because workers may still be starting up in the background.
-	"""
-	time.sleep(2.0)
-	start_time = time.time()
-	
-	results = ray.get([slow_function.remote(i) for i in range(4)])
-	
-	end_time = time.time()
-	duration = end_time - start_time
-	return results, duration
+  """Sleep a little to improve the accuracy of the timing measurements below.
+  We do this because workers may still be starting up in the background.
+  """
+  time.sleep(2.0)
+  start_time = time.time()
+  
+  results = ray.get([slow_function.remote(i) for i in range(4)])
+  
+  end_time = time.time()
+  duration = end_time - start_time
+  return results, duration
 
 
 def main():
-	ray.init('192.168.12.3:6349', redirect_output=True)
-	
-	# EXERCISE:
-	print(slow_function.remote(0))
-	print(ray.get(slow_function.remote(0)))
-	
-	# EXERCISE:
-	results, duration = measure_duration()
-	assert results == [0, 1, 2, 3], 'Did you remember to call ray.get?'
+  ray.init('192.168.12.3:6349', redirect_output=True)
+  
+  # EXERCISE:
+  print(slow_function.remote(0))
+  print(ray.get(slow_function.remote(0)))
+  
+  # EXERCISE:
+  results, duration = measure_duration()
+  assert results == [0, 1, 2, 3], 'Did you remember to call ray.get?'
   assert duration < 1.1, ('The loop took {} seconds. This is too slow.'.format(duration))
   assert duration > 1, ('The loop took {} seconds. This is too fast.'.format(duration))
-	print('Success! The example took {} seconds.'.format(duration))
-
-
-# EXERCISE:
-# Look assets/solution01.png
+  print('Success! The example took {} seconds.'.format(duration))
+  
+  # EXERCISE:
+  # Look assets/solution01.png
 
 
 if __name__ == '__main__':
-	main()
+  main()
